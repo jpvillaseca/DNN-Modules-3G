87957da1a8c3a36def251b48aea4b76dcfc49edc
Juan Pablo Villaseca <jpvillaseca@gmail.com>
2015-07-30 10:35:12 -0300

    missing files

---
+++ Services/SubscriptionValidationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Digevo.Infrastructure.Extensions;
+using System.Net;
+using System.IO;
+using Christoc.Modules.SubscriptionValidation.Components;
+
+namespace Christoc.Modules.SubscriptionValidation.Services
+{
+    public class SubscriptionValidationService
+    {
+        private static readonly string ValidationServiceEndpoint = "http://146.82.89.83/le/dnn/dnn_suscstatus.asp?ani={PhoneNumber}&productos={Products}";
+
+        public static async Task<bool> ValidateUserSubscriptionAsync(SubscriptionModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.PhoneNumber) || string.IsNullOrWhiteSpace(model.SerializedList))
+                return false;
+
+            var serviceCall = ValidationServiceEndpoint.FormatWith(
+                new { PhoneNumber = model.PhoneNumber, Products = model.SerializedList });
+
+            var request = (HttpWebRequest)WebRequest.Create(serviceCall);
+            var response = (HttpWebResponse)await request.GetResponseAsync();
+
+            bool isValidSubscription = false;
+
+            return bool.TryParse(new StreamReader(response.GetResponseStream()).ReadToEnd(), out isValidSubscription) ? isValidSubscription : false;
+        }
+    }
+}
